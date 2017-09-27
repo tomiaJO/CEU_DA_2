2242b46aec1312ee6771bc61cc6b4462fcc390be
tomiaJO <t.koncz@gmail.com>
2017-09-27 18:46:20 +0200

    -

--- HW_1_AMAZON.R
+++ HW_1_AMAZON.R
@@ -5,7 +5,9 @@
 
 
 ### Question 1.
-## Use the amazon_compare.csv file and choose a city to analyze. (Load it into Stata or R)
+## Q: Use the amazon_compare.csv file and choose a city to analyze. (Load it into Stata or R)
+
+## A: 
 df <- read.csv('amazon_compare.csv', header = TRUE)
 
 # head(df)
@@ -13,25 +15,66 @@ df <- read.csv('amazon_compare.csv', header = TRUE)
 # names(df)
 
 ### Question 2.
-## Drop observations where price or price online or both are missing. 
+## Q: Drop observations where price or price online or both are missing. 
+
 # summary(df$price) ## 0 NA values
 # summary(df$price_online) ## 1310 NA values
 # nrow(df) ##3992 rows in total
 # nrow(df[is.na(df$price_online) == FALSE, ]) ## 2682 rows in total where online price is not missing
 
-## creating a new df where NOT both variables are missing
-df_filtered = df[!(is.na(df$price_online) | is.na(df$price)), ] 
-
+## A: creating a new df where NOT both variables are missing
+df_filtered <- df[!(is.na(df$price_online) | is.na(df$price)), ] 
 # nrow(df_filtered) ## checking results ## numbers of rows are 2682 in the new dataset
 
+## Q: Drop observations that are larger than the 99-percentile.
+df_filtered <- df_filtered[df_filtered$price <= quantile(df_filtered$price, .99), ]
+df_filtered <- df_filtered[df_filtered$price_online <= quantile(df_filtered$price_online, .99), ]
 
-## Drop observations that are larger than the 99-percentile. 
+## Q: How do you call these observations? 
+## A: Outliers
+
+## Q: Make an argument on why should we keep them in the sample. 
+## A: 
+
+## Q: Make another argument on why should we drop them.
+## A: 
 
 
+### Question 3.
+## Q: Create a random number generator from a distribution of your own choice setting the seed equal
+## to your group number. Call this variable id_rand. 
 
-## How do you call these observations? 
-## Make an argument on why should we keep them in the sample. 
-## Make another argument on why should we drop them.
+## A: set seed to 11
+set.seed(11)
 
+## A: std normal distribution, mu = 0, sigma = 1, 10000 runs
+id_rand <- rnorm(10000, 0, 1)
 
-### Question 2
+## Q: Sort the variable and select the first 1000 observation in your dataset
+
+## A: ranked from highest to lowest, only picking the first 1000 numbers
+sort(id_rand, decreasing = TRUE)[1:1000]
+
+### Question 4.
+## Q: Make summary statistics (mean, standard deviations, quartiles, percentiles) of the variables price
+## and online price. 
+
+## A: mean
+mean(df_filtered$price)
+mean(df_filtered$price_online)
+
+## A: standard deviation
+sd(df_filtered$price)
+sd(df_filtered$price_online)
+
+## A: quartiles
+quantile(df_filtered$price, c(0, .25, .50, .75, 1))
+quantile(df_filtered$price_online, c(0, .25, .50, .75, 1))
+
+## A: percentiles
+pct <- seq(from = 0, to = 1, by = 0.01)
+quantile(df_filtered$price, pct)
+quantile(df_filtered$price_online, pct)
+
+## Q: Are there any differences between these statistics?
+## A: 
